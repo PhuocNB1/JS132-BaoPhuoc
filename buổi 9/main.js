9f9f8540e1be5a756666e048b00d683b33c65044
PhuocNB1 <baophuoc21@gmail.com>
2022-10-23 15:11:49 +0700

    FPT Play

---
+++ buổi 9/main.js
@@ -0,0 +1,11 @@
+let result = "";
+for(let a = 1; a < 12 ; a++){
+    for( let b = 1; b < 7; b++ ){
+        if(a >= 6){
+            if(b >= 7 - a){
+                result += "*"
+            }else
+        }
+    }
+}
+console.log(result);
